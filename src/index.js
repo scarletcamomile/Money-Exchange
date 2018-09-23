7a649d8c04df63fea098930a95b4ecc7aa5627e1
sugar-plum fairy <scarletcamomile@mail.ru>
2018-09-23 14:05:34 +0300

    better sol

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency > 10000) return {
+        error: "You are rich, my friend! We don't have so much coins for exchange"
+    };
+    if (currency <= 0) return {};
+    
+    const exchangeObj = {};
+    exchangeObj['H'] = H = ~~(currency / 50);
+    exchangeObj['Q'] = Q = ~~((currency - H * 50) / 25);
+    exchangeObj['D'] = D = ~~((currency - H * 50 - Q * 25) / 10);
+    exchangeObj['N'] = N = ~~((currency - H * 50 - Q * 25 - D * 10) / 5);
+    exchangeObj['P'] = P = currency - H * 50 - Q * 25 - D * 10 - N * 5;
+    for (coin in exchangeObj)
+        if (!exchangeObj[coin]) delete exchangeObj[coin];
+    return exchangeObj;
 }
